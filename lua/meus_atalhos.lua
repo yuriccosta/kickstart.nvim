b182ff1b5845025c90b7afc372f067b92bbc7020
yuriccosta <yuricoutinhoc@gmail.com>
2026-08-19 23:22:35 -0300

    atalho para salvar arquivo

--- lua/meus_atalhos.lua
+++ lua/meus_atalhos.lua
@@ -1,3 +1,6 @@
 -- Cria o atalho para copiar o arquivo inteiro
 vim.keymap.set('n', '<leader>ya', ':%y+<CR>', { desc = '[Y]ank [A]ll' })
 
+-- Salvar o arquivo atual rapidamente
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Salvar arquivo (:w)", silent = true })
+
